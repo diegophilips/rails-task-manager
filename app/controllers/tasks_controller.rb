c360d8be37108ce4c7f64aa1e08b05b76cf75f16
Diego Philips <diego.r.philips@gmail.com>
2020-11-10 15:06:17 +0000

    function but ugly

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    display_completed
+  end
+
+  def new
+  @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def update
+      @task.update(task_params)
+      redirect_to task_path(@task)
+  end
+
+   def edit
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def display_completed
+    @completed = if @task.completed == true
+    "This tasks is completed"
+    else
+    "You still have to do this"
+    end
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+     @task = Task.find(params[:id])
+  end
+end
+
